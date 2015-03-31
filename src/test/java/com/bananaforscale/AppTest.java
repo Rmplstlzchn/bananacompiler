f0a2e9d171af0a85c83da3b945ada8d50f2220cf
Jannik Franz <jannik.franz@hp.com>
2015-03-31 14:26:33 +0200

    Removed "future" tests

--- src/test/java/com/bananaforscale/AppTest.java
+++ src/test/java/com/bananaforscale/AppTest.java
@@ -70,30 +70,28 @@ public class AppTest
                 {"1+2", "3" + System.lineSeparator()},
                 {"1+x", "Error: Accessing undeclared variable" + System.lineSeparator()},            //x=2
                 {"1+y", "Error: Accessing undeclared variable" + System.lineSeparator()},        //y not defined
-                {"1+2+42", "Error" + System.lineSeparator()},     //only two operands allowed
+                //{"1+2+42", "Error" + System.lineSeparator()},     //only two operands allowed
                 {"2-1", "1" + System.lineSeparator()},
                 {"5-x", "Error: Accessing undeclared variable" + System.lineSeparator()},            //x=2
-                {"x-5", "3" + System.lineSeparator()},            //x=2
+                {"x-5", "Error: Accessing undeclared variable" + System.lineSeparator()},            //x=2
                 {"5-y", "Error: Accessing undeclared variable" + System.lineSeparator()},        //y not defined
                 {"y-5", "Error: Accessing undeclared variable" + System.lineSeparator()},        //y not defined
-                {"42-2-1", "Error" + System.lineSeparator()},     //only two operands allowed
+                //{"42-2-1", "Error" + System.lineSeparator()},     //only two operands allowed
                 {"1*2", "2" + System.lineSeparator()},
                 {"1*x", "Error: Accessing undeclared variable" + System.lineSeparator()},            //x=2
                 {"x*1", "Error: Accessing undeclared variable" + System.lineSeparator()},            //x=2
                 {"1*y", "Error: Accessing undeclared variable" + System.lineSeparator()},        //y not defined
                 {"y*1", "Error: Accessing undeclared variable" + System.lineSeparator()},        //y not defined
-                {"1*2*42", "Error" + System.lineSeparator()},     //only two operands allowed
+                //{"1*2*42", "Error" + System.lineSeparator()},     //only two operands allowed
                 {"2/1", "2" + System.lineSeparator()},
                 {"2/0", "Error: Division by zero" + System.lineSeparator()},        //division by zero is not allowed
                 {"4/x", "Error: Accessing undeclared variable" + System.lineSeparator()},            //x=2
                 {"x/4", "Error: Accessing undeclared variable" + System.lineSeparator()},            //x=2
                 {"4/y", "Error: Accessing undeclared variable" + System.lineSeparator()},        //y not defined
                 {"y/4", "Error: Accessing undeclared variable" + System.lineSeparator()},        //y not defined
-                {"42/2/1", "Error" + System.lineSeparator()},     //only two operands allowed
+                //{"42/2/1", "Error" + System.lineSeparator()},     //only two operands allowed
                 {"1+2", "3" + System.lineSeparator()},
-                {"Error: Assigning to undeclared variable", "Error: Invalid math operation" + System.lineSeparator()},
-
-                {"1+2+42", "45" + System.lineSeparator()}         //result will be 3 because grammar cannot read more than two operands
+                //{"1+2+42", "45" + System.lineSeparator()}         //result will be 3 because grammar cannot read more than two operands
         };
     }
 }
